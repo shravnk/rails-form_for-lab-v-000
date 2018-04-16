3080d7f76ac367ce648123269eacb624bc15ba7c
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-16 06:25:00 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,13 +10,13 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(student_params)
     @student.save
-    redirect_to students_path(@student)
+    redirect_to student_path(@student)
   end
 
   def update
     @student = find_student
     @student.update(student_params)
-    redirect_to students_path(@student)
+    redirect_to student_path(@student)
   end
 
   def edit
