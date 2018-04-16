37f48f6b7b329c248dc345e266044b0cd5d5c689
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-16 05:57:25 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,34 @@
 class SchoolClassesController < ApplicationController
+  def show
+    find_school_class
+  end
 
+  def index
+    @school_classs = school_class.all
+  end
+
+  def create
+    @school_class = school_class.new(school_class_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def update
+    @school_class = find_school_class
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_school_class
+    @school_class = school_class.find(params[:id])
+  end
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
 end
